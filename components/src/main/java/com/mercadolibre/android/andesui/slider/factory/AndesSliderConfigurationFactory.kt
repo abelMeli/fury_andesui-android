d8ffe826046ea52bf810ab223e17a8f81efcf227
Edgar <edgar.navarro@mercadolibre.com.co>
2022-09-23 13:06:12 -0500

    Slider a11y bug and crash (#623)
    * Accessibility fix and crash fixed
    
    * fix coverage.
    
    * fix coverage.
    
    * fix comments
    
    * fix comments
    
    Co-authored-by: enavarro42 <edgarnavarro42@gmail.com>

--- components/src/main/java/com/mercadolibre/android/andesui/slider/factory/AndesSliderConfigurationFactory.kt
+++ components/src/main/java/com/mercadolibre/android/andesui/slider/factory/AndesSliderConfigurationFactory.kt
@@ -3,7 +3,11 @@ package com.mercadolibre.android.andesui.slider.factory
 import android.content.Context
 import android.view.View
 import com.mercadolibre.android.andesui.slider.state.AndesSliderState
+import com.mercadolibre.android.andesui.slider.steps.AndesSliderSteps
 import com.mercadolibre.android.andesui.slider.type.AndesSliderType
+import com.mercadolibre.android.andesui.slider.utils.AndesSliderUtils.getCurrentStep
+import com.mercadolibre.android.andesui.slider.utils.AndesSliderUtils.getStepFrom
+import kotlin.math.roundToInt
 
 internal data class AndesSliderConfiguration(
     val text: String?,
@@ -23,19 +27,39 @@ internal object AndesSliderConfigurationFactory {
         return with(andesSliderAttrs) {
             val sliderType = andesSliderType.type
             val steps = andesSliderSteps.type
+            val jumpSteps = steps.stepSize(andesSliderMin, andesSliderMax)
 
             AndesSliderConfiguration(
-                    text = andesSliderTitle,
-                    state = andesSliderState,
-                    min = andesSliderMin,
-                    max = andesSliderMax,
-                    value = andesSliderValue,
-                    type = andesSliderType,
-                    rightComponent = sliderType.rightComponent(context),
-                    leftComponent = sliderType.leftComponent(context),
-                    isTickVisible = steps.isTickVisible(),
-                    jumpSteps = steps.stepSize(andesSliderMin, andesSliderMax)
+                text = andesSliderTitle,
+                state = andesSliderState,
+                min = andesSliderMin,
+                max = andesSliderMax,
+                value = resolveValue(andesSliderValue, andesSliderMin, jumpSteps, andesSliderSteps),
+                type = andesSliderType,
+                rightComponent = sliderType.rightComponent(context),
+                leftComponent = sliderType.leftComponent(context),
+                isTickVisible = steps.isTickVisible(),
+                jumpSteps = jumpSteps
             )
         }
+    }
+
+    private fun resolveValue(value: Float, min: Float, jumpSteps: Float, steps: AndesSliderSteps): Float {
+        return when (steps) {
+            AndesSliderSteps.None -> if (value < min) min else value
+            is AndesSliderSteps.Custom -> getValueFromCustom(value, min, jumpSteps, steps)
+        }
+    }
+
+    private fun getValueFromCustom(value: Float, min: Float, jumpSteps: Float, steps: AndesSliderSteps.Custom): Float {
+        var result = min
+
+        for (i in getStepFrom(min)..steps.type.getSteps()) {
+            val step = getCurrentStep(i, min, jumpSteps)
+            if (step.roundToInt() == value.roundToInt()) {
+                result = step
+            }
+        }
+        return result
     }
 }
